b2d02d668b0db86eb548b0afb9695881d316c7a6
jdjagath018-ux <jdjagath018@gmail.com>
2026-01-16 12:44:49 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello! Welcome to my repository.")
+import requests
+
+# This is the program to fetch your file from GitHub
+url = "https://raw.githubusercontent.com/jdjagath018-ux/new-git-hub-project/main/hello.py"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Success! File fetched from GitHub.")
+    print("Content of hello.py:")
+    print(response.text)
+else:
+    print("Failed to fetch file.")
